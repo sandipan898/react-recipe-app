e1cf26d737c52401141cb32ca1113ea7c6d61349
sandipan898 <sandipan.das898@gmail.com>
2021-02-04 07:58:12 +0530

    updating app.js

--- src/App.js
+++ src/App.js
@@ -2,22 +2,18 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
+
+  const APP_ID = '33995136';
+  const APP_KEY = '23af0629cc81af56228ee976e60320ed';
+  const API = `https://api.edamam.com/search?q=chicken&app_id=${APP_ID}&app_key=${APP_KEY}`
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Recipe App</h1>
+      <form className="search-form">
+        <input className="serach-bar" type="text" />
+        <button className="search-btn" type="submit">Search</button>
+      </form>
     </div>
   );
 }
